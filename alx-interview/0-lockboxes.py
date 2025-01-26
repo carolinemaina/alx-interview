95392179e9dab10d31195231eb76207d60e8fe30
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-01-27 00:24:24 +0300

    solution

---
+++ alx-interview/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    unlocked = {0}
+    keys = [0]
+    
+    while keys:
+        box = keys.pop()
+        for key in boxes[box]:
+            if key not in unlocked:
+                unlocked.add(key)
+                keys.append(key)
+
+    return len(unlocked) == len(boxes)
