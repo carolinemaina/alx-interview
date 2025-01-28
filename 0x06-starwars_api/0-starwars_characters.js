8cb26fec3c01913d287abf7d243fb022473d4eb5
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-01-28 20:14:53 +0300

    correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,27 @@
 #!/usr/bin/node
-const request = require("request");
-const ID = process.argv[2];
 
-if (!ID) {
-  console.log("Error: Missing movie ID argument.");
-  process.exit(1);
-}
+const request = require('request');
 
-const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
-
-function getCharacter(listChar, index) {
-  if (!Array.isArray(listChar)) {
-    console.log("Error: 'listChar' is not an array.");
-    return;
-  }
-
-  if (index === listChar.length) {
-    return;
-  }
-
-  request(`${listChar[index]}`, (error, response, body) => {
-    if (error) {
-      console.log(error);
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
     } else {
-      const character = JSON.parse(response.body);
-      console.log(character.name);
-      getCharacter(listChar, index + 1);
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
   });
-}
+};
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const film = JSON.parse(response.body);
-    const listChar = film.characters;
-
-    if (Array.isArray(listChar)) {
-      getCharacter(listChar, 0);
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
     } else {
-      console.log("Error: 'characters' is not an array or is undefined.");
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
     }
   }
-});
+);
