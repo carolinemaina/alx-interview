82fccdcbe9b05c37ed57f8f72c0414ae10f1570f
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-11 14:50:19 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,27 @@
 #!/usr/bin/node
 
-const request = require('request');
-
-// Check if a movie ID is provided
-const movieId = process.argv[2];
-if (!movieId) {
-	console.log('Usage: ./0-starwars_characters.js <Movie ID>');
-	process.exit(1);
-}
-
-const url = `https://swapi-api.alx-tools.com/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-	if (error) {
-		console.log('Error:', error);
-		return;
-	}
-
-	if (response.statusCode !== 200) {
-		console.log('Failed to fetch data. Status Code:', response.statusCode);
-		return;
-	}
-
-	const movieData = JSON.parse(body);
-	const characterUrls = movieData.characters;
-
-	// Iterate over the character URLs and fetch each character's name
-	characterUrls.forEach((characterUrl) => {
-		request(characterUrl, (charError, charResponse, charBody) => {
-			if (charError) {
-				console.log('Error fetching character:', charError);
-				return;
-			}
-
-			if (charResponse.statusCode !== 200) {
-				console.log('Failed to fetch character. Status Code:', charResponse.statusCode);
-				return;
-			}
-
-			const characterData = JSON.parse(charBody);
-			console.log(characterData.name);
+function makeRequest (url) {
+	const request = require('request');
+	return new Promise((resolve, reject) => {
+		request.get(url, (error, response, body) => {
+			if (error) reject(error);
+			else resolve(JSON.parse(body));
 		});
 	});
-});
+}
+
+async function main () {
+	const args = process.argv;
+
+	if (args.length < 3) return;
+	const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+	const movie = await makeRequest(movieUrl);
+
+	if (movie.characters === undefined) return;
+	for (const characterUrl of movie.characters) {
+		const character = await makeRequest(characterUrl);
+		console.log(character.name);
+	}
+}
+
+main();
